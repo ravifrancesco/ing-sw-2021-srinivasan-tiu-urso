2fcc2a090fd05765bfaa878837cc122e0a2120ed
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-22 21:44:04 +0100

    Added achieved to VaticanReport.java

--- src/main/java/it/polimi/ingsw/model/VaticanReport.java
+++ src/main/java/it/polimi/ingsw/model/VaticanReport.java
@@ -9,11 +9,21 @@ public class VaticanReport {
 
     protected final int winningPoints;
 
+    private boolean achieved;
+
     public VaticanReport(int start, int end, int popeSpace, int winningPoints) {
         this.start = start;
         this.end = end;
         this.popeSpace = popeSpace;
         this.winningPoints = winningPoints;
+        this.achieved = false;
     }
 
+    public void achieve() {
+        this.achieved = true;
+    }
+
+    public boolean isAchieved() {
+        return achieved;
+    }
 }
