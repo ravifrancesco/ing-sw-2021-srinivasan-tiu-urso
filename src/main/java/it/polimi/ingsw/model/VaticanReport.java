0369b3387d6acce2b288d1078b0461814a84fa05
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-22 22:16:05 +0100

    Added reached in VaticanReport.java

--- src/main/java/it/polimi/ingsw/model/VaticanReport.java
+++ src/main/java/it/polimi/ingsw/model/VaticanReport.java
@@ -7,20 +7,30 @@ public class VaticanReport {
 
     protected final int popeSpace;
 
-    protected final int winningPoints;
+    protected final int victoryPoints;
 
+    private boolean reached;
     private boolean achieved;
 
-    public VaticanReport(int start, int end, int popeSpace, int winningPoints) {
+    public VaticanReport(int start, int end, int popeSpace, int victoryPoints) {
         this.start = start;
         this.end = end;
         this.popeSpace = popeSpace;
-        this.winningPoints = winningPoints;
+        this.victoryPoints = victoryPoints;
+        this.reached = false;
         this.achieved = false;
+    }
+
+    public void reach() {
+        this.reached = true;
     }
 
     public void achieve() {
         this.achieved = true;
+    }
+
+    public boolean isReached() {
+        return reached;
     }
 
     public boolean isAchieved() {
