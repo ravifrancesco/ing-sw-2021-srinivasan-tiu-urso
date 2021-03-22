c2c51fc44f7bda9ced7d19c4beeb5fa59a26c3cf
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-22 17:22:01 +0100

    Added doc for marble classes

--- src/main/java/it/polimi/ingsw/model/BlueMarble.java
+++ src/main/java/it/polimi/ingsw/model/BlueMarble.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.model;
 
 public class BlueMarble implements Marble {
 
+    /**
+     * @see it.polimi.ingsw.model.Marble#getResource(it.polimi.ingsw.model.Player)
+     */
     @Override
     public Resource getResource(Player p) {
         return null;
