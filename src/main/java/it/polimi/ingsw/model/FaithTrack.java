b114ef6f7973003e7e4d48a88d8ed6ca44b6f648
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-23 18:23:54 +0100

    Completed FaithTrack.java and VaticanReport.java and added doc

--- src/main/java/it/polimi/ingsw/model/FaithTrack.java
+++ src/main/java/it/polimi/ingsw/model/FaithTrack.java
@@ -1,41 +1,107 @@
 package it.polimi.ingsw.model;
 
-import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * This class represents the Faith Track presents in the user dashboard.
+ * The object memorizes the state of the Faith Track. The state includes:
+ * <ul>
+ * <li> The red cross position.
+ * <li> The vatican report zones.
+ * <li> The bonus victory points in the faith track.
+ * <li> The total winning points accumulated by the faith track.
+ * </ul>
+ * <p>
+ */
 public class FaithTrack {
 
 	static final int LENGTH = 25;
 
 	private int position;
 
-	private VaticanReport[] vaticanReports;
+	private Map<Integer, VaticanReport> vaticanReports;
 	private Map<Integer, Integer> faithTrackVictoryPoints;
 
 	private int winningPoints;
 
+	/**
+	 * The constructor for a FaithTrack object.
+	 * It initializes the winning points and the user position
+	 * and load the vatican reports settings and the faith track
+	 * bonus victory points from the gameSettings object.
+	 *
+	 * @param gameSettings the settings for the current game.
+	 */
 	public FaithTrack(GameSettings gameSettings) {
 		this.winningPoints = 0;
 		this.position = 0;
-		this.vaticanReports = gameSettings.getVaticanReports();
+		this.vaticanReports = gameSettings.getVaticanReports().stream()
+				.collect(Collectors.toMap(v -> v.end, VaticanReport::clone));
 		this.faithTrackVictoryPoints = gameSettings.getFaithTrackVictoryPoints();
 	}
 
-	public boolean moveFaithMarker(int pos) {
-		for (int i = 0; i < pos; i++) {
-			position++;
-			winningPoints += faithTrackVictoryPoints.get(position) == null ? 0 : faithTrackVictoryPoints.get(position);
-		}
-
-		Arrays.stream(vaticanReports).filter(v -> v.start <= position).forEach(VaticanReport::reach);
-
+	/**
+	 * Resets the state of this object to the initial state.
+	 */
+	public void reset() {
+		this.winningPoints = 0;
+		this.position = 0;
+		vaticanReports.values().forEach(VaticanReport::reset);
 	}
 
+	/**
+	 * Moves the red cross by pos positions on the faith track, while
+	 * checking and eventually adding if the cross steps on a bonus cell.
+	 *
+	 * @param pos	number of positions for the movement.
+	 * @return		<code>true</code> if the player has reached the
+	 * 				last cell;
+	 * 				<code>false</code> otherwise.
+	 */
+	public boolean moveFaithMarker(int pos) {
+		for (int i = 0; i < pos; i++) {
+			if (position == LENGTH) {
+				return true;
+			} else {
+				position++;
+				winningPoints += faithTrackVictoryPoints.get(position) == null ? 0 : faithTrackVictoryPoints.get(position);
+			}
+		}
+		return false;
+	}
 
+	/**
+	 * To be called when a player reaches a vatican checkpoint cell.
+	 * This methods checks if the cross has reached the start of
+	 * the vatican report zone described by the checkpoint that is
+	 * passes to the method. In case it has, it will add the points
+	 * to the winningPoints and set the vaticanReport to achieved,
+	 * otherwise it will set the vatican report to missed.
+	 *
+	 * @param vaticanReportEnd checkpoint of the vatican report zone
+	 *                         to be checked.
+	 */
+	public void getVaticanVictoryPoints(int vaticanReportEnd) {
+		VaticanReport currentVaticanReport = vaticanReports.get(vaticanReportEnd);
+		if (position < currentVaticanReport.start) { currentVaticanReport.isMissed(); }
+		else { currentVaticanReport.isAchieved(); }
+	}
+
+	/**
+	 * Getter for the position
+	 *
+	 * @return the current red cross position
+	 */
 	public int getPosition() {
 		return position;
 	}
 
+	/**
+	 * Getter for the winningPoints
+	 *
+	 * @return the current winningPoints
+	 */
 	public int getWinningPoints() {
 		return winningPoints;
 	}
--- src/main/java/it/polimi/ingsw/model/VaticanReport.java
+++ src/main/java/it/polimi/ingsw/model/VaticanReport.java
@@ -1,39 +1,97 @@
 package it.polimi.ingsw.model;
 
-public class VaticanReport {
+/**
+ * This class represents a Vatican Report Zone that is contained in the
+ * Faith Track. It contains variables describing the vatican report zone.
+ * The object memorizes the state of the Vatican Report Zone. The state includes:
+ * <ul>
+ * <li> A boolean "missed" that describes if the victory point card was discarded.
+ * <li> A boolean "achieve" that describes if the victory point card was flipped.
+ * </ul>
+ * <p>
+ */
+public class VaticanReport implements Cloneable {
 
     protected final int start;
     protected final int end;
 
-    protected final int popeSpace;
-
     protected final int victoryPoints;
 
-    private boolean reached;
+    private boolean missed;
     private boolean achieved;
 
-    public VaticanReport(int start, int end, int popeSpace, int victoryPoints) {
+    /**
+     * The constructor for a VaticanReport object. It sets the missed and achieved
+     * to false.
+     *
+     * @param start             the start of the vatican report zone.
+     * @param end               the end of the vatican report zone (also the checkpoint).
+     * @param victoryPoints     the victory points obtained if the the card is flipped.
+     */
+    public VaticanReport(int start, int end, int victoryPoints) {
         this.start = start;
         this.end = end;
-        this.popeSpace = popeSpace;
         this.victoryPoints = victoryPoints;
-        this.reached = false;
-        this.achieved = false;
+        this.missed = false;
+        this.achieved = true;
     }
 
-    public void reach() {
-        this.reached = true;
+    /**
+     * Resets the object to the initial state: missed and achieved are set to false.
+     */
+    public void reset() {
+        this.missed = false;
+        this.achieved = true;
     }
 
+    /**
+     * To be called if another player reaches the checkpoint for this zone
+     * before this player has reached the start of the zone. missed is set
+     * to false, representing the bonus victory points card being discarded.
+     */
+    public void miss() {
+        this.missed = true;
+    }
+
+    /**
+     * To be called if another player reaches the checkpoint for this zone
+     * and this player has reached the start of the zone. achieved is set
+     * to true, representing the bonus victory points card being flipped.
+     */
     public void achieve() {
         this.achieved = true;
     }
 
-    public boolean isReached() {
-        return reached;
+    /**
+     * Getter for missed.
+     *
+     * @return  <code>true</code> if the player has discarded
+     *          the bonus victory points card;
+     *          <code>false</code> otherwise.
+     */
+    public boolean isMissed() {
+        return missed;
     }
 
+    /**
+     * Getter for achieved.
+     *
+     * @return  <code>true</code> if the player has flipped
+     *          the bonus victory points card;
+     *          <code>false</code> otherwise.
+     */
     public boolean isAchieved() {
         return achieved;
     }
+
+    /**
+     * Clones the current object
+     *
+     * @return a clone of <code>this</code>
+     */
+    @Override
+    protected VaticanReport clone() {
+        return new VaticanReport(start, end, victoryPoints);
+    }
+
 }
