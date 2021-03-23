ac6f78944e8ad4e9b899b737cb852517a2409372
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-23 19:28:41 +0100

    Added doc for GameSettings.java

--- src/main/java/it/polimi/ingsw/model/GameSettings.java
+++ src/main/java/it/polimi/ingsw/model/GameSettings.java
@@ -6,11 +6,23 @@ import java.util.List;
 import java.util.Properties;
 import java.util.stream.IntStream;
 
+/**
+ * This class represents the settings for the current game.
+ * The object memorizes the modifiable settings of the game.
+ * The state includes this custom settings:
+ * <ul>
+ * <li> The development cards.
+ * <li> the leader card.
+ * <li> The production power of the dashboard.
+ * <li> The vatican reports.
+ * <li> The faith track victory points.
+ * </ul>
+ * <p>
+ */
 public class GameSettings {
 
     static final int DEVELOPMENT_CARD_NUM = 40;
     static final int LEADER_CARD_NUM = 16;
-    static final int VATICAN_REPORTS_NUM = 3;
     static final int FAITH_TRACK_LENGTH = 25;
 
     private DevelopmentCard[] developmentCards;
@@ -23,6 +35,39 @@ public class GameSettings {
 
     private int[] faithTrackVictoryPoints;
 
+    /**
+     * The constructor for a GameSettings object.
+     *
+     * @param developmentCards          array of DEVELOPMENT_CARD_NUM developmentCards.
+     * @param leaderCards               array of LEADER_CARD_NUM leaderCards.
+     * @param dashBoardProductionPower  productionPower of the dashboard.
+     * @param vaticanReports            List of vaticanReports.
+     * @param faithTrackVictoryPoints   array of FAITH_TRACK_LENGTH int representing faithTrackVictoryPoints.
+     */
+    public GameSettings(DevelopmentCard[] developmentCards, LeaderCard[] leaderCards, ProductionPower dashBoardProductionPower,
+                        List<VaticanReport> vaticanReports, int[] faithTrackVictoryPoints) {
+        this.developmentCards = developmentCards;
+        this.leaderCards = leaderCards;
+        this.dashBoardProductionPower = dashBoardProductionPower;
+        this.vaticanReports = vaticanReports;
+        this.faithTrackVictoryPoints = faithTrackVictoryPoints;
+    }
+
+    /**
+     * Construtor to use to create GameSettings from .properties file.
+     *
+     * @param p path of the .properties file.
+     */
+    public GameSettings(Path p) {
+        loadSettings(p);
+    }
+
+
+    /**
+     * Saves the current settings to a .properties file described by a path.
+     *
+     * @param p path to locate where to save the .properties file.
+     */
     public void saveSettings(Path p) {
 
         try {
@@ -54,22 +99,47 @@ public class GameSettings {
         // TODO after implementing constructors for cards and production power
     }
 
+    /**
+     * Getter for developmentCards.
+     *
+     * @return an array of DEVELOPMENT_CARD_NUM development cards.
+     */
     public DevelopmentCard[] getDevelopmentCards() {
         return developmentCards;
     }
 
+    /**
+     * Getter for leaderCards.
+     *
+     * @return an array of LEADER_CARD_NUM leader cards.
+     */
     public LeaderCard[] getLeaderCards() {
         return leaderCards;
     }
 
+    /**
+     * Getter for dashBoardProductionPower.
+     *
+     * @return the dashBoardProductionPower.
+     */
     public ProductionPower getDashBoardProductionPower() {
         return dashBoardProductionPower;
     }
 
+    /**
+     * Getter for developmentCards.
+     *
+     * @return a List of vaticanReports.
+     */
     public List<VaticanReport> getVaticanReports() {
         return vaticanReports;
     }
 
+    /**
+     * Getter for faithTrackVictoryPoints.
+     *
+     * @return array of FAITH_TRACK_LENGTH int representing faithTrackVictoryPoints.
+     */
     public int[] getFaithTrackVictoryPoints() {
         return faithTrackVictoryPoints;
     }
