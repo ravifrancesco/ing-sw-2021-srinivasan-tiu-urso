eadbf14d26dd2ccb4bf13d4ea5cf348fb861976d
ursogiuseppe <giuseppeurso07@gmail.com>
2021-03-28 16:27:04 +0200

    Created Deck class.

--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -1,13 +1,36 @@
 package it.polimi.ingsw.model;
 
-import java.util.Collection;
+import java.util.*;
 
 public class Deck {
 
-	private Collection<Card> card;
+	private Stack<DevelopmentCard> developmentCardDeck;
+	private Stack<LeaderCard> leaderCardDeck;
+	private List<Card> discardDeck;
+
+	Deck(GameSettings gameSettings){
+		developmentCardDeck=new Stack<>();
+		leaderCardDeck=new Stack<>();
+		discardDeck=new ArrayList<>();
+		Arrays.stream(gameSettings.getDevelopmentCards()).forEach(c->developmentCardDeck.push(c));
+		Arrays.stream(gameSettings.getLeaderCards()).forEach(c->leaderCardDeck.push(c));
+	}
 
 	public void shuffle() {
+		Collections.shuffle(developmentCardDeck);
+		Collections.shuffle(leaderCardDeck);
+	}
 
+	public void discard(Card c){
+		discardDeck.add(c);
+	}
+
+	public LeaderCard getLeaderCard(){
+		return !leaderCardDeck.empty() ? leaderCardDeck.pop() : null;
+	}
+
+	public DevelopmentCard getDevelopmentCard(){
+		return !developmentCardDeck.empty() ? developmentCardDeck.pop() : null;
 	}
 
 }
