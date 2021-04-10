7c0d9eb295723e29c3e79195ab0ade1db41b1e97
ursogiuseppe <giuseppeurso07@gmail.com>
2021-04-10 10:33:56 +0200

    Added doc to DevelopmentCardDiscount class. Created DevelopmentCardDiscountTest class.

--- src/main/java/it/polimi/ingsw/model/DevelopmentCardDiscount.java
+++ src/main/java/it/polimi/ingsw/model/DevelopmentCardDiscount.java
@@ -1,23 +1,49 @@
 package it.polimi.ingsw.model;
 
+/**
+ * The class represents the special ability of a leader card which allows to buy a development card
+ * from the grid with a discount.
+ */
+
 public class DevelopmentCardDiscount implements SpecialAbility {
 
 	private Resource resource;
 	private int quantity;
+
+	/**
+	 * The constructor for a DevelopmentCardDiscount object.
+	 * @param resource the resource associated with the discount.
+	 * @param quantity the quantity of the discount.
+	 */
 
 	public DevelopmentCardDiscount(Resource resource, int quantity) {
 		this.resource = resource;
 		this.quantity = quantity;
 	}
 
+	/**
+	 * Activate the discount special ability. It adds this object to the discount collection of the player.
+	 * @param p the player who activated the discount.
+	 */
+
 	@Override
 	public void activate(Player p) {
 		p.addActiveDiscount(this);
 	}
 
+	/**
+	 * Getter for the resource.
+	 * @return the resource associated with the discount.
+	 */
+
 	public Resource getResource() {
 		return resource;
 	}
+
+	/**
+	 * Getter for the quantity.
+	 * @return the quantity of the discount.
+	 */
 
 	public int getQuantity() {
 		return quantity;
