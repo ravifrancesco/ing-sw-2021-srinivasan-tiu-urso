be8d54dd3b8c0d4f465fd1b8b83fd415a9506a45
ursogiuseppe <giuseppeurso07@gmail.com>
2021-04-29 15:04:18 +0200

    DevelopmentCard doc and test fixed.

--- src/test/java/it/polimi/ingsw/model/cards/DevelopmentCardTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/DevelopmentCardTest.java
@@ -2,16 +2,40 @@ package it.polimi.ingsw.model.cards;
 
 import it.polimi.ingsw.model.*;
 import it.polimi.ingsw.model.cards.DevelopmentCard;
-import it.polimi.ingsw.model.specialAbilities.ProductionPower;
+import it.polimi.ingsw.model.specialAbilities.*;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class DevelopmentCardTest {
+
+    @Test
+    public void constructorTest() {
+        Banner banner = new Banner(BannerEnum.BLUE, 2);
+
+        Map<Resource, Integer> resourceCost = new HashMap<>();
+        resourceCost.put(Resource.SERVANT, 3);
+
+        Map<Resource, Integer> resourceRequired = new HashMap<>();
+        resourceRequired.put(Resource.GOLD, 1);
+        resourceRequired.put(Resource.STONE, 1);
+
+        Map<Resource, Integer> resourceProduced = new HashMap<>();
+        resourceProduced.put(Resource.SHIELD, 1);
+
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
+
+        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
+
+        Assert.assertEquals(c.getId(), 1);
+        Assert.assertEquals(c.getVictoryPoints(), 5);
+        Assert.assertEquals(c.getBanner().getColor(), BannerEnum.BLUE);
+        Assert.assertEquals(c.getBanner().getLevel(), 2);
+        Assert.assertEquals(c.getResourceCost(), resourceCost);
+        Assert.assertEquals(c.getProductionPower(), p);
+    }
 
     @Test
     public void toStringTest() {
@@ -27,21 +51,18 @@ public class DevelopmentCardTest {
         Map<Resource, Integer> resourceProduced = new HashMap<>();
         resourceProduced.put(Resource.SHIELD, 1);
 
-        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2, false);
-
-        Map<Resource, Integer> resourceCostDC = new HashMap<>();
-        resourceCostDC.put(Resource.SERVANT, 3);
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
 
         DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
 
-        String s1 = "ID=1;VP=5;BA=BLUE:2;RC=SERVANT:3;SA=PP;RR=STONE:1,GOLD:1;RP=SHIELD:1;FP=2;SR=n;";
-        String s2 = "ID=1;VP=5;BA=BLUE:2;RC=SERVANT:3;SA=PP;RR=GOLD:1,STONE:1;RP=SHIELD:1;FP=2;SR=n;";
+        String s1 = "ID=1;VP=5;BA=BLUE:2;RC=SERVANT:3;SA=PP;RR=STONE:1,GOLD:1;RP=SHIELD:1;FP=2;";
+        String s2 = "ID=1;VP=5;BA=BLUE:2;RC=SERVANT:3;SA=PP;RR=GOLD:1,STONE:1;RP=SHIELD:1;FP=2;";
 
         Assert.assertTrue(s1.equals(c.toString()) || s2.equals(c.toString()));
     }
 
     @Test
-    public void getterTest() {
+    public void equalsTrueTest() {
         Banner banner = new Banner(BannerEnum.BLUE, 2);
 
         Map<Resource, Integer> resourceCost = new HashMap<>();
@@ -54,88 +75,162 @@ public class DevelopmentCardTest {
         Map<Resource, Integer> resourceProduced = new HashMap<>();
         resourceProduced.put(Resource.SHIELD, 1);
 
-        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2, false);
-
-        Map<Resource, Integer> resourceCostDC = new HashMap<>();
-        resourceCostDC.put(Resource.SERVANT, 3);
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
 
         DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
+        DevelopmentCard c2 = new DevelopmentCard(1, 5, resourceCost, p, banner);
 
-        Assert.assertEquals(c.getId(), 1);
-        Assert.assertEquals(c.getVictoryPoints(), 5);
-        Assert.assertEquals(c.getBanner().getColor(), BannerEnum.BLUE);
-        Assert.assertEquals(c.getBanner().getLevel(), 2);
-        Assert.assertEquals(c.getResourceCost(), resourceCost);
-        Assert.assertEquals(c.getProductionPower(), p);
+        Assert.assertTrue(c.equals(c2));
     }
 
     @Test
-    public void playTest() {
-        // TODO after implementing Dashboard class
-    }
+    public void equalsFalseTest() {
+        Banner banner = new Banner(BannerEnum.BLUE, 2);
 
-    @Test
-    public void movingFaithMarkerTest() {
-        Map<Resource, Integer> resourceCostPP = new HashMap<>();
-        resourceCostPP.put(Resource.GOLD, 1);
-        resourceCostPP.put(Resource.STONE, 1);
+        Map<Resource, Integer> resourceCost = new HashMap<>();
+        resourceCost.put(Resource.SERVANT, 3);
+
+        Map<Resource, Integer> resourceRequired = new HashMap<>();
+        resourceRequired.put(Resource.GOLD, 1);
+        resourceRequired.put(Resource.STONE, 1);
 
         Map<Resource, Integer> resourceProduced = new HashMap<>();
         resourceProduced.put(Resource.SHIELD, 1);
 
-        ProductionPower p = new ProductionPower(resourceCostPP, resourceProduced,2, false);
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
 
-        Map<Resource, Integer> resourceCostDC = new HashMap<>();
-        resourceCostDC.put(Resource.SERVANT, 3);
+        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
 
-        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCostDC, p, null);
+        Banner banner2 = new Banner(BannerEnum.GREEN, 1);
+
+        Map<Resource, Integer> resourceProduced2 = new HashMap<>();
+        resourceProduced2.put(Resource.SHIELD, 1);
+
+        ProductionPower p2 = new ProductionPower(resourceRequired, resourceProduced2,1);
+
+        DevelopmentCard c2 = new DevelopmentCard(2, 3, resourceCost, p2, banner2);
+
+        Assert.assertFalse(c.equals(c2));
+    }
+
+    @Test
+    public void activateTest() {
+        Banner banner = new Banner(BannerEnum.BLUE, 2);
+
+        Map<Resource, Integer> resourceCost = new HashMap<>();
+        resourceCost.put(Resource.SERVANT, 3);
+
+        Map<Resource, Integer> resourceRequired = new HashMap<>();
+        resourceRequired.put(Resource.GOLD, 1);
+        resourceRequired.put(Resource.STONE, 1);
+
+        Map<Resource, Integer> resourceProduced = new HashMap<>();
+        resourceProduced.put(Resource.SHIELD, 1);
+
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
+
+        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
+
+        GameSettings gameSettings = buildGameSettings();
+        Player player = new Player(gameSettings);
+        player.reset();
+
+        Optional<Map<Resource, Integer>> emptyOptional = Optional.empty();
+
+        p.setSelectableResource(emptyOptional, emptyOptional);
+
+        Map<Resource, Integer> playerResources = new HashMap<>();
+
+        playerResources.put(Resource.GOLD, 2);
+        playerResources.put(Resource.STONE, 3);
+        playerResources.put(Resource.SHIELD, 1);
+
+        playerResources.forEach((key, value) -> player.getDashboard().storeResourceInLocker(key, value));
+
+        c.activate(player);
+
+        playerResources = player.getDashboard().getAllPlayerResources();
+
+        Assert.assertEquals(player.getDashboard().getFaithMarkerPosition(), 2);
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.SERVANT)), Optional.of(0));
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.GOLD)), Optional.of(2));
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.STONE)), Optional.of(3));
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.SHIELD)), Optional.of(2));
+    }
+
+    @Test
+    public void resetProductionPowerTest() {
+        Banner banner = new Banner(BannerEnum.BLUE, 2);
+
+        Map<Resource, Integer> resourceCost = new HashMap<>();
+        resourceCost.put(Resource.SERVANT, 3);
+
+        Map<Resource, Integer> resourceRequired = new HashMap<>();
+        resourceRequired.put(Resource.GOLD, 1);
+        resourceRequired.put(Resource.STONE, 1);
+
+        Map<Resource, Integer> resourceProduced = new HashMap<>();
+        resourceProduced.put(Resource.SHIELD, 1);
+
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
+
+        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
+
+        GameSettings gameSettings = buildGameSettings();
+        Player player = new Player(gameSettings);
+        player.reset();
+
+        Optional<Map<Resource, Integer>> emptyOptional = Optional.empty();
+
+        p.setSelectableResource(emptyOptional, emptyOptional);
+
+        Map<Resource, Integer> playerResources = new HashMap<>();
+
+        playerResources.put(Resource.GOLD, 2);
+        playerResources.put(Resource.STONE, 3);
+        playerResources.put(Resource.SHIELD, 1);
+
+        playerResources.forEach((key, value) -> player.getDashboard().storeResourceInLocker(key, value));
+
+        c.activate(player);
+
+        c.resetProductionPower();
+
+        c.activate(player);
+
+        Assert.assertEquals(player.getDashboard().getFaithMarkerPosition(), 2);
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.SERVANT)), Optional.of(0));
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.GOLD)), Optional.of(2));
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.STONE)), Optional.of(3));
+        Assert.assertEquals(Optional.ofNullable(playerResources.get(Resource.SHIELD)), Optional.of(3));
+    }
+
+    private GameSettings buildGameSettings() {
+
+
+        DevelopmentCard[] developmentCards = developmentCardDeckBuilder();
+
+        int leaderCardNum = 16;
+        LeaderCard[] leaderCards = leaderCardDeckBuilder(leaderCardNum);
+
+        ProductionPower dashboardProductionPower = productionPowerBuilder();
+
+        int vaticanReportsNum = 3;
+        List<VaticanReport> vaticanReports = vaticanReportsListBuilder();
 
         int[] faithTrackVictoryPoints = {0,0,0,1,0,0,2,0,0,4,0,0,6,0,0,9,0,0,12,0,0,16,0,0,0};
 
-        List<VaticanReport> vaticanReports = new ArrayList<>();
+        return new GameSettings(developmentCards, leaderCardNum, leaderCards,
+                dashboardProductionPower, vaticanReports, faithTrackVictoryPoints);
 
-        vaticanReports.add(new VaticanReport(5,8,2));
-        vaticanReports.add(new VaticanReport(12,15,2));
-        vaticanReports.add(new VaticanReport(19,24,4));
-
-
-        GameSettings gameSettings = new GameSettings(null, 0, null, null, vaticanReports, faithTrackVictoryPoints);
-
-        FaithTrack faithTrack = new FaithTrack(gameSettings);
-
-        Dashboard dashboard = new Dashboard(gameSettings, null);
-        Player player = new Player("nickname", "AAA123");
-
-        player.setDashboard(dashboard);
-        dashboard.setPlayer(player);
-
-        c.play(dashboard, 0);
-        c.activate(player);
-
-        Assert.assertEquals(dashboard.getFaithMarkerPosition(),2);
     }
 
-    @Test
-    public void storeResourcesTest() {
-        // TODO after implementing Warehouse class
-    }
-
-    @Test
-    public void storingAndFMTest() {
-        // TODO after implementing Warehouse class
-    }
-
-    @Test
-    public void giveSelectableResourceTest() {
-        // TODO after implementing Dashboard and Warehouse classes
-    }
-
-    @Test
-    public void trueIsActivatableTest() {
-        Banner banner = new Banner(BannerEnum.BLUE, 2);
+    private DevelopmentCard[] developmentCardDeckBuilder() {
 
         Map<Resource, Integer> resourceCost = new HashMap<>();
-        resourceCost.put(Resource.SERVANT, 3);
+        resourceCost.put(Resource.SERVANT, 2);
+        resourceCost.put(Resource.GOLD, 1);
+        resourceCost.put(Resource.SHIELD, 1);
 
         Map<Resource, Integer> resourceRequired = new HashMap<>();
         resourceRequired.put(Resource.GOLD, 1);
@@ -144,27 +239,30 @@ public class DevelopmentCardTest {
         Map<Resource, Integer> resourceProduced = new HashMap<>();
         resourceProduced.put(Resource.SHIELD, 1);
 
-        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2, false);
+        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2);
 
-        Map<Resource, Integer> resourceCostDC = new HashMap<>();
-        resourceCostDC.put(Resource.SERVANT, 3);
+        return IntStream.range(0, GameSettings.DEVELOPMENT_CARD_NUM)
+                .boxed()
+                .map(i -> new DevelopmentCard(i, 4, resourceCost, p, chooseBanner(i)))
+                .toArray(DevelopmentCard[]::new);
 
-        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
-
-        Map<Resource, Integer> playerResources = new HashMap<>();
-        playerResources.put(Resource.GOLD, 2);
-        playerResources.put(Resource.SERVANT, 1);
-        playerResources.put(Resource.STONE, 3);
-
-        Assert.assertTrue(c.isActivatable(playerResources));
     }
 
-    @Test
-    public void falseIsActivatableTest() {
-        Banner banner = new Banner(BannerEnum.BLUE, 2);
+    private LeaderCard[] leaderCardDeckBuilder(int leaderCardNum) {
 
-        Map<Resource, Integer> resourceCost = new HashMap<>();
-        resourceCost.put(Resource.SERVANT, 3);
+        Map<Banner, Integer> bannerCost = new HashMap<>();
+
+        bannerCost.put(new Banner(BannerEnum.GREEN, 1), 2);
+        bannerCost.put(new Banner(BannerEnum.BLUE, 2), 1);
+
+        return  IntStream.range(0, leaderCardNum)
+                .boxed()
+                .map(i -> new LeaderCard(i, 2, bannerCost, specialAbilityBuilder(i)))
+                .toArray(LeaderCard[]::new);
+
+    }
+
+    private ProductionPower productionPowerBuilder() {
 
         Map<Resource, Integer> resourceRequired = new HashMap<>();
         resourceRequired.put(Resource.GOLD, 1);
@@ -173,18 +271,76 @@ public class DevelopmentCardTest {
         Map<Resource, Integer> resourceProduced = new HashMap<>();
         resourceProduced.put(Resource.SHIELD, 1);
 
-        ProductionPower p = new ProductionPower(resourceRequired, resourceProduced,2, false);
+        return new ProductionPower(resourceRequired, resourceProduced,2);
 
-        Map<Resource, Integer> resourceCostDC = new HashMap<>();
-        resourceCostDC.put(Resource.SERVANT, 3);
+    }
 
-        DevelopmentCard c = new DevelopmentCard(1, 5, resourceCost, p, banner);
+    private List<VaticanReport> vaticanReportsListBuilder() {
 
-        Map<Resource, Integer> playerResources = new HashMap<>();
-        playerResources.put(Resource.GOLD, 1);
-        playerResources.put(Resource.SERVANT, 1);
-        playerResources.put(Resource.SHIELD, 3);
+        List<VaticanReport> vaticanReportsList = new ArrayList<>();
+        vaticanReportsList.add(new VaticanReport(5, 8, 2));
+        vaticanReportsList.add(new VaticanReport(12, 16, 3));
+        vaticanReportsList.add(new VaticanReport(19, 24, 4));
 
-        Assert.assertFalse(c.isActivatable(playerResources));
+        return vaticanReportsList;
+
+
+    }
+
+    private SpecialAbility specialAbilityBuilder(int i) {
+
+        Map<Resource, Integer> resourceRequired = new HashMap<>();
+        resourceRequired.put(Resource.GOLD, 1);
+
+        Map<Resource, Integer> resourceProduced = new HashMap<>();
+        resourceProduced.put(Resource.ANY, 1);
+
+        return switch (i % 4) {
+            case 1 ->
+                    new DevelopmentCardDiscount(Resource.GOLD, 1);
+            case 2 ->
+                    new WarehouseExtraSpace(Resource.SHIELD);
+            case 3 ->
+                    new WhiteMarbleResource(Resource.STONE);
+            default ->
+                    new ProductionPower(resourceRequired, resourceProduced, 1);
+        };
+    }
+
+    private Banner chooseBanner(int val) {
+        return switch (val % 12) {
+            case 0 -> new Banner(BannerEnum.GREEN, 1);
+            case 1 -> new Banner(BannerEnum.GREEN, 2);
+            case 2 -> new Banner(BannerEnum.GREEN, 3);
+            case 3 -> new Banner(BannerEnum.YELLOW, 1);
+            case 4 -> new Banner(BannerEnum.YELLOW, 2);
+            case 5 -> new Banner(BannerEnum.YELLOW, 3);
+            case 6 -> new Banner(BannerEnum.BLUE, 1);
+            case 7 -> new Banner(BannerEnum.BLUE, 2);
+            case 8 -> new Banner(BannerEnum.BLUE, 3);
+            case 9 -> new Banner(BannerEnum.PURPLE, 1);
+            case 10 -> new Banner(BannerEnum.PURPLE, 2);
+            case 11 -> new Banner(BannerEnum.PURPLE, 3);
+            default -> null;
+        };
+    }
+
+    private int getRow(int val) {
+        return switch (val % 12) {
+            case 0, 1, 2, 3 -> 1;
+            case 4, 5, 6, 7 -> 2;
+            case 8, 9, 10, 11 -> 3;
+            default -> -1;
+        };
+    }
+
+    private int getColumn(int val) {
+        return switch (val % 12) {
+            case 0, 4, 8 -> 1;
+            case 1, 5, 9 -> 2;
+            case 2, 6, 10 -> 3;
+            case 3, 7, 11 -> 4;
+            default -> -1;
+        };
     }
 }
