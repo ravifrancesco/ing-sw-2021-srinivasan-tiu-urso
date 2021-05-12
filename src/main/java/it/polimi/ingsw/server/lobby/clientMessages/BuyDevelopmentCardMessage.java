4237ef71d7633d51da390b3718b3277bf7717f50
Giuseppe Urso <giuseppeurso07@gmail.com>
2021-05-12 11:19:13 +0200

    Create message classes giuseppe (#87)
    * Test
    
    * Done my message classes.
    
    * Updated packages.

---
+++ src/main/java/it/polimi/ingsw/server/lobby/clientMessages/BuyDevelopmentCardMessage.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.server.lobby.clientMessages;
+
+import it.polimi.ingsw.controller.ServerController;
+import it.polimi.ingsw.model.ResourceContainer;
+import it.polimi.ingsw.server.Connection;
+import it.polimi.ingsw.server.lobby.clientMessages.ClientMessage;
+
+public class BuyDevelopmentCardMessage implements ClientMessage {
+
+    int row;
+    int column;
+    ResourceContainer resourceToPayCost;
+    int position;
+
+    public BuyDevelopmentCardMessage(int row, int column, ResourceContainer resourceToPayCost, int position) {
+        this.row = row;
+        this.column = column;
+        this.resourceToPayCost = resourceToPayCost;
+        this.position = position;
+    }
+
+    @Override
+    public void handle(Connection c, ServerController serverController) {
+        try {
+            serverController.buyDevelopmentCard(c.getNickname(), row, column, resourceToPayCost, position);
+        } catch (Exception e) {
+            // TODO
+        }
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/server/lobby/clientMessages/ClientMessage.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.server.lobby.clientMessages;
+
+import it.polimi.ingsw.controller.ServerController;
+import it.polimi.ingsw.server.Connection;
+
+public interface ClientMessage {
+
+    void handle(Connection connection, ServerController serverController);
+}
