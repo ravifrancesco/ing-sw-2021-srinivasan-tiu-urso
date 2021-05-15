1ee98f4f052301fac7f0c6368b59a007ca350bb7
rbta-svg <roberttiu83@gmail.com>
2021-05-15 11:42:46 +0200

    Completed Game tests with 100% coverage

---
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -0,0 +1,115 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class GameTest {
+
+    @Test
+    public void gameConstructorTest() {
+        Game game = new Game("1");
+        Assert.assertNotNull(game.getGameId());
+        Assert.assertNotNull(game.getPlayers());
+        Assert.assertNotNull(game.getGameBoard());
+    }
+
+    @Test
+    public void loadGameSettingsTest() {
+        Game game = new Game("1");
+        GameSettingsBuilder gameSettingsBuilder = new GameSettingsBuilder();
+        GameSettings gameSettings = gameSettingsBuilder.build();
+        Assert.assertNull(game.getGameSettings());
+        game.loadGameSettings(gameSettings);
+        Assert.assertNotNull(game.getGameSettings());
+    }
+
+    @Test
+    public void resetTest() {
+        // TODO find a better way to test this?
+        Game game = new Game("1");
+        GameSettingsBuilder gameSettingsBuilder = new GameSettingsBuilder();
+        GameSettings gameSettings = gameSettingsBuilder.build();
+        game.loadGameSettings(gameSettings);
+        Assert.assertFalse(game.checkEnd());
+        game.endGame();
+        Assert.assertTrue(game.checkEnd());
+        game.reset();
+        Assert.assertFalse(game.checkEnd());
+    }
+
+    @Test
+    public void checkWinner() {
+        Game game = new Game("1");
+        GameSettingsBuilder gameSettingsBuilder = new GameSettingsBuilder();
+        GameSettings gameSettings = gameSettingsBuilder.build();
+        game.loadGameSettings(gameSettings);
+        game.getPlayers().put("rbta-svg", new Player(gameSettings));
+        game.getPlayers().put("ravifrancesco", new Player(gameSettings));
+        game.getPlayers().put("giuseppeurso", new Player(gameSettings));
+        game.getPlayers().get("rbta-svg").getDashboard().placeLeaderCard(gameSettings.getLeaderCards()[0]);
+        game.getPlayers().get("rbta-svg").getDashboard().placeLeaderCard(gameSettings.getLeaderCards()[1]);
+        game.getPlayers().get("ravifrancesco").getDashboard().placeLeaderCard(gameSettings.getLeaderCards()[3]);
+        game.getPlayers().get("ravifrancesco").getDashboard().placeLeaderCard(gameSettings.getLeaderCards()[4]);
+        game.getPlayers().get("giuseppeurso").getDashboard().placeLeaderCard(gameSettings.getLeaderCards()[5]);
+
+        game.getGameStatus();
+
+        // game.getPlayers().entrySet().forEach((e ->
+        // System.out.println("Player " + e.getKey() + " has " + e.getValue().getVictoryPoints())));
+
+        Player winner = game.checkWinner();
+        Assert.assertEquals(winner, game.getPlayers().get("rbta-svg"));
+    }
+
+    @Test
+    public void addPlayerTest() {
+        Game game = new Game("1");
+        GameSettingsBuilder gameSettingsBuilder = new GameSettingsBuilder();
+        GameSettings gameSettings = gameSettingsBuilder.build();
+        game.loadGameSettings(gameSettings);
+        Assert.assertEquals(game.getPlayers().values().size(), 0);
+        game.addPlayer("rbta-svg", new Player(gameSettings));
+        Assert.assertEquals(game.getPlayers().values().size(), 1);
+
+
+    }
+
+    @Test
+    public void getNextPlayerTest() {
+        Game game = new Game("1");
+        GameSettingsBuilder gameSettingsBuilder = new GameSettingsBuilder();
+        GameSettings gameSettings = gameSettingsBuilder.build();
+        game.loadGameSettings(gameSettings);
+        game.addPlayer("rbta-svg", new Player(gameSettings));
+        game.addPlayer("ravifrancesco", new Player(gameSettings));
+        game.addPlayer("giuseppeurso", new Player(gameSettings));
+
+        Assert.assertEquals(game.getNextPlayer(), "rbta-svg");
+        Assert.assertEquals(game.getNextPlayer(), "ravifrancesco");
+        Assert.assertEquals(game.getNextPlayer(), "giuseppeurso");
+        Assert.assertEquals(game.getNextPlayer(), "rbta-svg");
+        Assert.assertEquals(game.getNextPlayer(), "ravifrancesco");
+        Assert.assertEquals(game.getNextPlayer(), "giuseppeurso");
+
+
+    }
+
+
+    @Test
+    public void getTurnPhaseTest() {
+        Game game = new Game("1");
+        GameSettingsBuilder gameSettingsBuilder = new GameSettingsBuilder();
+        GameSettings gameSettings = gameSettingsBuilder.build();
+        game.loadGameSettings(gameSettings);
+
+        Assert.assertNull(game.getTurnPhase());
+        game.startUniquePhase(TurnPhase.FIRST_TURN);
+        Assert.assertEquals(game.getTurnPhase(), TurnPhase.FIRST_TURN);
+
+
+
+    }
+
+
+}
