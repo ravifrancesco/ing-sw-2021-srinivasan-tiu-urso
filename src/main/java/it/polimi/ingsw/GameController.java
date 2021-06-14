e75c7ee56428670cd3f4558277688b27bcd41830
ursogiuseppe <giuseppeurso07@gmail.com>
2021-06-14 20:01:27 +0200

    Created Resource Controller (still working).

--- src/main/java/it/polimi/ingsw/GameController.java
+++ src/main/java/it/polimi/ingsw/GameController.java
@@ -1,6 +1,10 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.Resource;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -40,82 +44,25 @@ public class GameController {
             pane.getChildren().add(rectangles[i]);
         }
 
-        pane.getChildren().add(getResource(100, 100, "coin"));
-        pane.getChildren().add(getResource(200, 200, "servant"));
-        pane.getChildren().add(getResource(300, 300, "shield"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/resource_item.fxml"));
+        try {
+            Node node = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ResourceController resourceController = loader.getController();
+        resourceController.assignSlots(rectangles);
+        resourceController.createItem(Resource.GOLD);
+        resourceController.setX(100);
+        resourceController.setY(100);
 
+        pane.getChildren().add(resourceController.getItem());
     }
 
     @FXML
     private void testClick(MouseEvent event) {
         System.out.println(event.getX());
         System.out.println(event.getY());
-    }
-
-    private ImageView getResource(int x, int y, String resourceType) {
-        File file = new File("src/main/resources/png/"+resourceType+".png");
-        Image image = new Image(file.toURI().toString());
-        ImageView resource = new ImageView(image);
-
-        resource.setX(x);
-        resource.setY(y);
-        resource.setFitHeight(25);
-        resource.setFitWidth(25);
-        resource.setOnMousePressed(event -> pressed(event, resource));
-        resource.setOnMouseDragged(event -> dragged(event, resource));
-        resource.setOnMouseReleased(event -> released(event, resource));
-
-        return resource;
-    }
-
-    private void pressed(MouseEvent event, ImageView resource) {
-        ColorAdjust colorAdjust = new ColorAdjust();
-        colorAdjust.setBrightness(-0.5);
-        resource.setEffect(colorAdjust);
-    }
-
-    private void dragged(MouseEvent event, ImageView resource) {
-        resource.setX(event.getX()-12.5);
-        resource.setY(event.getY()-12.5);
-    }
-
-    private void released(MouseEvent event, ImageView resource) {
-        ColorAdjust colorAdjust = new ColorAdjust();
-        colorAdjust.setBrightness(0);
-        resource.setEffect(colorAdjust);
-        computePosition(resource);
-    }
-
-    private void computePosition(ImageView resource) {
-        double x = resource.getX() + 12.5;
-        double y = resource.getY() + 12.5;
-        if (y >= rectangles[0].getY() && y <= rectangles[1].getY() + rectangles[0].getHeight() &&
-            x >= rectangles[0].getX() && x <= rectangles[0].getX() + rectangles[0].getWidth()) {
-            resource.setX((rectangles[0].getX()+rectangles[0].getWidth()/2)-(resource.getFitWidth()/2));
-            resource.setY((rectangles[0].getY()+rectangles[0].getHeight()/2)-(resource.getFitWidth()/2));
-        }
-        if (y >= rectangles[1].getY() && y <= rectangles[1].getY() + rectangles[1].getHeight()) {
-            if (x >= rectangles[1].getX() && x <= rectangles[1].getX() + rectangles[1].getWidth()) {
-                resource.setX((rectangles[1].getX()+rectangles[1].getWidth()/2)-(resource.getFitWidth()/2));
-                resource.setY((rectangles[1].getY()+rectangles[1].getHeight()/2)-(resource.getFitWidth()/2));
-            } else if (x >= rectangles[2].getX() && x <= rectangles[2].getX() + rectangles[2].getWidth()) {
-                resource.setX((rectangles[2].getX()+rectangles[2].getWidth()/2)-(resource.getFitWidth()/2));
-                resource.setY((rectangles[2].getY()+rectangles[2].getHeight()/2)-(resource.getFitWidth()/2));
-            }
-        }
-        else if (y >= rectangles[3].getY() && y <= rectangles[3].getY() + rectangles[3].getHeight()) {
-            if (x >= rectangles[3].getX() && x <= rectangles[3].getX() + rectangles[3].getWidth()) {
-                resource.setX((rectangles[3].getX()+rectangles[3].getWidth()/2)-(resource.getFitWidth()/2));
-                resource.setY((rectangles[3].getY()+rectangles[3].getHeight()/2)-(resource.getFitWidth()/2));
-            } else if (x >= rectangles[4].getX() && x <= rectangles[4].getX() + rectangles[4].getWidth()) {
-                resource.setX((rectangles[4].getX()+rectangles[4].getWidth()/2)-(resource.getFitWidth()/2));
-                resource.setY((rectangles[4].getY()+rectangles[4].getHeight()/2)-(resource.getFitWidth()/2));
-            } else if (x >= rectangles[5].getX() && x <= rectangles[5].getX() + rectangles[5].getWidth()) {
-                resource.setX((rectangles[5].getX()+rectangles[5].getWidth()/2)-(resource.getFitWidth()/2));
-                resource.setY((rectangles[5].getY()+rectangles[5].getHeight()/2)-(resource.getFitWidth()/2));
-            }
-        }
-
     }
 
 }
---
+++ src/main/java/it/polimi/ingsw/ResourceController.java
@@ -0,0 +1,112 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.Resource;
+import javafx.fxml.FXML;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+import java.io.File;
+
+public class ResourceController {
+
+    @FXML ImageView item;
+
+    double x;
+
+    double y;
+
+    Resource resourceType;
+
+    Rectangle[] slots;
+
+    public void assignSlots(Rectangle[] slots) {
+        this.slots = slots;
+    }
+
+    public void createItem(Resource resourceType) {
+        String name = "";
+        this.resourceType = resourceType;
+        switch(resourceType) {
+            case GOLD -> name = "coin";
+            case SHIELD -> name = "shield";
+            case STONE -> name = "stone";
+            case SERVANT -> name = "servant";
+        }
+        File file = new File("src/main/resources/png/"+name+".png");
+        Image image = new Image(file.toURI().toString());
+        item = new ImageView(image);
+        item.setFitHeight(25);
+        item.setFitWidth(25);
+        item.setOnMousePressed(this::pressed);
+        item.setOnMouseDragged(this::dragged);
+        item.setOnMouseReleased(this::released);
+    }
+
+    public void setX(double x) {
+        this.x = x;
+        item.setX(this.x);
+    }
+
+    public void setY(double y) {
+        this.y = y;
+        item.setY(this.y);
+    }
+
+    public ImageView getItem() {
+        return this.item;
+    }
+
+    @FXML
+    public void pressed(MouseEvent event) {
+        ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setBrightness(-0.5);
+        item.setEffect(colorAdjust);
+    }
+
+    private void dragged(MouseEvent event) {
+        item.setX(event.getX()-item.getFitWidth()/2);
+        item.setY(event.getY()-item.getFitHeight()/2);
+    }
+
+    private void released(MouseEvent event) {
+        ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setBrightness(0);
+        item.setEffect(colorAdjust);
+        computePosition();
+    }
+
+    private void computePosition() {
+        double x = this.x + item.getFitWidth()/2;
+        double y = this.y + item.getFitHeight()/2;
+        if (y >= slots[0].getY() && y <= slots[1].getY() + slots[0].getHeight() &&
+                x >= slots[0].getX() && x <= slots[0].getX() + slots[0].getWidth()) {
+            setX((slots[0].getX()+slots[0].getWidth()/2)-(item.getFitWidth()/2));
+            setY((slots[0].getY()+slots[0].getHeight()/2)-(item.getFitWidth()/2));
+        }
+        if (y >= slots[1].getY() && y <= slots[1].getY() + slots[1].getHeight()) {
+            if (x >= slots[1].getX() && x <= slots[1].getX() + slots[1].getWidth()) {
+                setX((slots[1].getX()+slots[1].getWidth()/2)-(item.getFitWidth()/2));
+                setY((slots[1].getY()+slots[1].getHeight()/2)-(item.getFitWidth()/2));
+            } else if (x >= slots[2].getX() && x <= slots[2].getX() + slots[2].getWidth()) {
+                setX((slots[2].getX()+slots[2].getWidth()/2)-(item.getFitWidth()/2));
+                setY((slots[2].getY()+slots[2].getHeight()/2)-(item.getFitWidth()/2));
+            }
+        }
+        else if (y >= slots[3].getY() && y <= slots[3].getY() + slots[3].getHeight()) {
+            if (x >= slots[3].getX() && x <= slots[3].getX() + slots[3].getWidth()) {
+                setX((slots[3].getX()+slots[3].getWidth()/2)-(item.getFitWidth()/2));
+                setY((slots[3].getY()+slots[3].getHeight()/2)-(item.getFitWidth()/2));
+            } else if (x >= slots[4].getX() && x <= slots[4].getX() + slots[4].getWidth()) {
+                setX((slots[4].getX()+slots[4].getWidth()/2)-(item.getFitWidth()/2));
+                setY((slots[4].getY()+slots[4].getHeight()/2)-(item.getFitWidth()/2));
+            } else if (x >= slots[5].getX() && x <= slots[5].getX() + slots[5].getWidth()) {
+                setX((slots[5].getX()+slots[5].getWidth()/2)-(item.getFitWidth()/2));
+                setY((slots[5].getY()+slots[5].getHeight()/2)-(item.getFitWidth()/2));
+            }
+        }
+    }
+}
