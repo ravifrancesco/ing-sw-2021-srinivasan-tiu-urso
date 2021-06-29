ef8f1ec810cf5432fa9dc04e6af58526f66381cc
ursogiuseppe <giuseppeurso07@gmail.com>
2021-06-29 22:42:47 +0200

    Done doc of special abilities.

--- src/main/java/it/polimi/ingsw/model/full/specialAbilities/SpecialAbility.java
+++ src/main/java/it/polimi/ingsw/model/full/specialAbilities/SpecialAbility.java
@@ -2,9 +2,21 @@ package it.polimi.ingsw.model.full.specialAbilities;
 
 import it.polimi.ingsw.model.full.table.Player;
 
+/**
+ * The interface represent a special ability of a game's card.
+ */
+
 public interface SpecialAbility {
 
+	/**
+	 * Allows to activate the special ability.
+	 * @param 	p represents the player.
+	 */
 	void activate(Player p);
 
+	/**
+	 * Allows to know the type of special ability.
+	 * @return	the special ability type.
+	 */
 	SpecialAbilityType getType();
 }
